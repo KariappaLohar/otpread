abc03658d6d0b59fb8efff765cff63aded9fe99d
15karan <klohar453@gmail.com>
2020-10-31 23:42:15 +0530

    removed dummy lib

--- app/src/main/java/com/kariappa/readmessage/MainActivity.java
+++ app/src/main/java/com/kariappa/readmessage/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.kariappa.mylibrary.MyClass;
 import com.kariappa.readmsg.MessageClass;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MessageClass.RunPermission(this);
     }
 }
