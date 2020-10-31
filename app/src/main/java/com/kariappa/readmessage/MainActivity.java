89408a4d185271db4721013658f2b26a84c5a245
15karan <klohar453@gmail.com>
2020-10-31 23:56:32 +0530

    removed dummy new

--- app/src/main/java/com/kariappa/readmessage/MainActivity.java
+++ app/src/main/java/com/kariappa/readmessage/MainActivity.java
@@ -3,17 +3,27 @@ package com.kariappa.readmessage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.kariappa.mylibrary.MyClass;
 import com.kariappa.readmsg.MessageClass;
+import com.kariappa.readmsg.MessageListener;
+import com.kariappa.readmsg.MessageReceiver;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MessageListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        MessageReceiver.bindListener(this);
         MessageClass.RunPermission(this);
+    }
+
+    @Override
+    public void messageReceived(int message) {
+
+        Toast.makeText(this, "NUmber"+message, Toast.LENGTH_SHORT).show();
+
     }
 }
